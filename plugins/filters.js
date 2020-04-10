0bf40e8b94966cb5c9e56bc91257f0ce9827bb6c
TP-WenpinLiao <wenpin.liao@thinkpower.com.tw>
2020-04-10 15:54:54 +0800

    modify marked table and highlight settings

--- plugins/filters.js
+++ plugins/filters.js
@@ -5,18 +5,22 @@ import pathHelper from '~/helpers/path'
 
 // Create your custom renderer.
 const renderer = new Renderer()
-renderer.code = (code, language) => {
-  // Check whether the given language is valid for highlight.js.
-  const validLang = !!(language && highlightjs.getLanguage(language))
-  // Highlight only if the language is valid.
-  const highlighted = validLang ? highlightjs.highlight(language, code).value : code
-  // Render the highlighted code with `hljs` class.
-  return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`
-}
 renderer.link = (href, title, text) => `<a target="_blank" href="${href}" title="${title}">${text}</a>`
+renderer.table = (header, body) => `<table class="table table-striped">${header}${body}</table>`
 
 // Set the renderer to marked.
-marked.setOptions({ renderer, baseUrl: pathHelper.getBaseUrl() })
+marked.setOptions({
+  renderer,
+  baseUrl: pathHelper.getBaseUrl(),
+  highlight: function (code, language) {
+    // Check whether the given language is valid for highlight.js.
+    const validLang = !!(language && highlightjs.getLanguage(language))
+    // Highlight only if the language is valid.
+    const highlighted = validLang ? highlightjs.highlight(language, code).value : code
+    // Render the highlighted code with `hljs` class.
+    return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`
+  }
+})
 
 Vue.filter('parseMd', function (content) {
   return marked(content)
