f6628d9a4edabfafb6648d9826e4eb507db3d6fc
TP-WenpinLiao <wenpin.liao@tpisoftware.com>
2020-07-14 09:49:25 +0800

    add scss function mixins,移除webpack重覆設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -158,16 +158,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-
+    extend (config) {
       config.plugins.push(
         new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /(en|zh-tw)$/),
         new webpack.ProvidePlugin({
